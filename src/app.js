d6717eee3507758d96753eb2fe42ef29eb43011b
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-09-29 08:05:10 -0700

    SAVEPOINT

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const { NODE_ENV, CORS_ORIGIN } = require('../../src/config/envConfig');
+const { NODE_ENV, CORS_ORIGIN } = require('../src/config/envConfig');
 
-const errors = require('../../src/middlewares/errors');
+const errors = require('../src/middlewares/errors');
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
@@ -26,6 +26,8 @@ app.get('/', (req, res) => {
 /*
 | ROUTES HERE -------------------------
 */
+
+
 
 /*
 |--------------------------------------
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const knex = require('knex');
 
 const app = require('./app');
-const logger = require('../../src/libs/logger');
-const { PORT, DB_URL } = require('../../src/config/envConfig');
+const logger = require('../src/libs/logger');
+const { PORT, DATABASE_URL } = require('../src/config/envConfig');
 
 const db = knex({
   client: 'pg',
-  connection: DB_URL
+  connection: DATABASE_URL
 })
 app.set('db', db)
 
