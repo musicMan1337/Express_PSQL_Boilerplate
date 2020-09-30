918682b84acd8c2c45f2c46d431ce8e7490b7e0a
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-09-30 08:12:43 -0700

    SAVEPOINT

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   CORS_ORIGIN: process.env.CORS_ORIGIN,
   DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
+  TEST_DB_URL: process.env.TEST_DB_URL
 };
