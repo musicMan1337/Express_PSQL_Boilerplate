d4413598d8c843aa6f47ebeb6389832b731a510d
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-10-04 18:50:50 -0700

    SAVEPOINT

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,7 @@
-const { NODE_ENV, TEST_DB_URL, DB_URL } = require('./src/config/envConfig');
+const { NODE_ENV, TEST_DB_URL, DATABASE_URL } = require('./src/config');
 
 module.exports = {
-  migrationDirectory: __dirname + './db/migrations',
+  migrationDirectory: __dirname + '/db/migrations',
   driver: 'pg',
-  connectionString: NODE_ENV === 'test' ? TEST_DB_URL : DB_URL
+  connectionString: NODE_ENV === 'test' ? TEST_DB_URL : DATABASE_URL
 };
