a4d81ac89bcf56f397faa61ef503d8cc607503d3
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-11-24 15:07:13 -0700

    SAVEPOINT

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ const { app, error } = require('./middlewares');
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 const morganSkip = { skip: () => NODE_ENV === 'test' };
 const corsOrigin = {
-  origin: NODE_ENV === 'production' ? CORS_ORIGIN_DEV : CORS_ORIGIN_PROD
+  origin: NODE_ENV === 'production' ? CORS_ORIGIN_PROD : CORS_ORIGIN_DEV
 };
 
 app.use(morgan(morganOption, morganSkip));
 app.use(cors(corsOrigin));
 app.use(helmet());
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Express boilerplate initialized!');
 });
 
